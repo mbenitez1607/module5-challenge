c791cea333493f9581a974bdb9dad57f1626f9e5
mbenitez <mbenitez.a@gmail.com>
2023-02-18 18:22:07 -0500

    Implement retrieve tasks functionality

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ function saveTask(id) {
     let task = document.getElementById(id);
     localStorage.setItem(id, task.value);
 }
+
+// Function to get all the tasks from local storage and display (on page reload)
+function getTasks() {
+    for (i = 900; i <= 1700; i += 100) {
+        console.log(i);
+        let task = localStorage.getItem(i);
+        // If there's a saved task for a specific time, retrieve and display 
+        if ( task != null) {
+            document.getElementById(i).value = task;
+        } else {
+            document.getElementById(i).value = "";
+        }
+    }
+}
+
+getTasks();
