903e1b05ddcde0b14b1f4e9690d858503496f8b3
mbenitez <mbenitez.a@gmail.com>
2023-02-18 17:43:53 -0500

    Implement save task functionality

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 // Set today's date with Moment
 let today = moment().format('dddd, MMMM do');
 $("#currentDay").text(today);
+
+// Function to save a task to local storage
+function saveTask(id) {
+    let task = document.getElementById(id);
+    localStorage.setItem(id, task.value);
+}
