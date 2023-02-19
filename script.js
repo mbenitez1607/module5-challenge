4a47b433b358fda613b989ac64d956c363c67320
mbenitez <mbenitez.a@gmail.com>
2023-02-18 19:22:02 -0500

    Implement coloring functionality based on time

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ $("#currentDay").text(today);
 // Function to save a task to local storage
 function saveTask(id) {
     let task = document.getElementById(id);
-    localStorage.setItem(id, task.value);
+    // Only store tasks that have content
+    if (task.value !== "") {
+        localStorage.setItem(id, task.value);
+    }
 }
 
 // Function to get all the tasks from local storage and display (on page reload)
 function getTasks() {
     for (i = 900; i <= 1700; i += 100) {
-        console.log(i);
         let task = localStorage.getItem(i);
         // If there's a saved task for a specific time, retrieve and display 
         if ( task != null) {
@@ -22,4 +24,24 @@ function getTasks() {
     }
 }
 
+// Function to determine whether a time-block is in past, present, or future
+function timeCheck() {
+    let currentTime = moment().format('H') * 100;
+    currentTime = 1100;
+    for (i = 900; i <= 1700; i += 100) {
+        let inputI = document.getElementById(i);
+        // CurrentTime is earlier than the time-block
+        if (currentTime < i) {
+            inputI.setAttribute("class","future textarea h-100");
+        // CurrentTime is later than the time-block
+        } else if (currentTime > i) {
+            inputI.setAttribute("class","past textarea h-100");
+        // CurrentTime is now
+        } else {
+            inputI.setAttribute("class","present textarea h-100");
+        }
+    }
+}
+
 getTasks();
+timeCheck();
